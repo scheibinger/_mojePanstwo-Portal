1d6f2ba10090146777d338858726804d0b7b9bd8
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-02-10 16:25:35 +0100

    KRS

--- app/Plugin/Krs/Controller/OrganizacjeController.php
+++ app/Plugin/Krs/Controller/OrganizacjeController.php
@@ -12,57 +12,26 @@ class OrganizacjeController extends AppController
     	
     	$groups = $this->API->KRS()->getFeaturedOrganizationsByGroups();
     	$this->set('groups', $groups);
-    	
-    	/*
-        $org = @$this->params->query['org'];
-        $osb = @$this->params->query['osb'];
-        $this->set('org', $org);
-        $this->set('osb', $osb);
-        $results = false;
 
-        if ($org) {
-            $conditions = array(
-                'org' => $org,
-                'dataset' => 'krs_podmioty',
-            );
-
-            $searcher = $this->API->Dane();
-            $searcher->searchDataset('krs_podmioty', array(
-                'conditions' => $conditions,
-                'limit' => 5,
-            ));
-
-            $organization = $searcher->getObjects();
-            if (!empty($organization))
-                $results = true;
-
-            $this->set('organization', $organization);
-
-        }elseif($osb){
-            $conditions = array(
-                'osb' => $osb,
-                'dataset' => 'krs_podmioty',
-            );
-
-            $searcher = $this->API->Dane();
-            $searcher->searchDataset('krs_podmioty', array(
-                'conditions' => $conditions,
-                'limit' => 5,
-            ));
-
-            $osoba = $searcher->getObjects();
-            if (!empty($osoba))
-                $results = true;
-
-            $this->set('osoba', $osoba);
-        }
-
-        $this->set('results', $results);
-
-        $application = $this->getApplication();
-        $this->set('title_for_layout', $application['Application']['name']);
-        */
-
+    }
+    
+    public function search()
+    {
+	    if( isset($this->request->params['ext']) && ($this->request->params['ext'] == 'json') )
+	    {
+	    	
+	    	$search = array();
+	    
+		    $q = @$this->request->query['q'];
+		    if( $q )
+			    $search = $this->API->KRS()->search($q);
+			    		    
+			$search = @$search['search'];
+			
+		    $this->set('search', $search);
+		    $this->set('_serialize', array('search'));
+		    
+	    } else $this->redirect('/krs');
     }
 
 } 
