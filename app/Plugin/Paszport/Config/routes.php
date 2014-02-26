7a73baab1a39e8d017cf43381463466fc94e6a98
Cain-Ish <mariusz.machuta@gmail.com>
2014-02-26 12:56:30 +0100

    #68

--- app/Plugin/Paszport/Config/routes.php
+++ app/Plugin/Paszport/Config/routes.php
@@ -3,7 +3,6 @@
 Router::connect('/paszport', array('plugin' => 'paszport', 'controller' => 'pages', 'action' => 'home'));
 
 Router::connect('/authorize', array('plugin' => 'paszport', 'controller' => 'passports', 'action' => 'authorize'));
-Router::connect('/login', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'failed'));
-Router::connect('/zaloguj', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'failed'));
-Router::connect('/logowanie', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'failed'));
-//Router::redirect('/login', array('plugin' => 'paszport', 'controller' => 'passport', 'action' => 'login'));
+Router::connect('/login', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'login'));
+Router::redirect('/zaloguj', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'login'));
+Router::redirect('/logowanie', array('plugin' => 'paszport', 'controller' => 'users', 'action' => 'login'));
