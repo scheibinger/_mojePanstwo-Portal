ed10062659bc4e37c58ef1b1cafa10ce3fc80e55
Cain <mariusz.machuta@gmail.com>
2014-03-06 15:36:13 +0100

    first shape of D3 + graph

--- app/Plugin/Dane/webroot/js/view-krspodmioty.js
+++ app/Plugin/Dane/webroot/js/view-krspodmioty.js
@@ -67,7 +67,7 @@ jQuery(document).ready(function () {
         menu = jQuery('.objectsPageContent .objectMenu'),
         menuAutoScroll = true,
         headerHeight = jQuery('header').outerHeight(),
-        sys = null,
+        connectionGraph = jQuery('#connectionGraph'),
         dataHighlightsOptions = jQuery('.dataHighlightsOptions');
 
     if (banner.length > 0) {
@@ -135,161 +135,101 @@ jQuery(document).ready(function () {
         }
     }).scroll();
 
-    /*jQuery.ajax({
-     url: "graph.json",
-     type: "GET",
-     beforeSend: function () {
-     sys = arbor.ParticleSystem();
-     sys.parameters({stiffness: 900, repulsion: 2000, gravity: true, dt: 0.015});
-     sys.renderer = Renderer("#connectionGraph");
-     },
-     success: function (data) {
-     jQuery.each(data.nodes, function () {
-     if (this.label == 'podmiot') {
-     sys.addNode(this.id, {'color': 'blue', 'shape': 'dot', 'label': this.label, 'data': this.data, radius: .3});
-     } else if (this.label == 'osoba') {
-     sys.addNode(this.id, {'color': 'green', 'shape': 'dot', 'label': this.label, 'data': this.data, radius: .3});
-     }
-     });
-     jQuery.each(data.relationships, function () {
-     sys.addEdge(this.start, this.end, {'data': this.data});
-     });
-     }
-     });*/
-});
-
-(function ($) {
-    var Renderer = function (elt) {
-        var dom = $(elt);
-        var canvas = dom.get(0);
-        var gfx = arbor.Graphics(canvas);
-        var sys = null;
-
-        var selected = null,
-            nearest = null,
-            _mouseP = null;
-
-
-        var that = {
-            init: function (pSystem) {
-                sys = pSystem;
-                sys.screen({size: {width: dom.width(), height: dom.height()},
-                    padding: [20, 0, 20, 0]});
-
-                $(window).resize(that.resize);
-                that.resize();
-                that._initMouseHandling();
+    if (connectionGraph.length > 0) {
+        var d3Data = {
+            'width': connectionGraph.outerWidth(),
+            'height': 500,
+            'color': {
+                'links': '#333333',
+                'podmiot': "#FF0000",
+                'osoba': "#0000FF"
             },
-            resize: function () {
-                canvas.width = dom.width();
-                canvas.height = '500';
-                sys.screen({size: {width: canvas.width, height: canvas.height}});
-                that.redraw();
-            },
-            redraw: function () {
-                gfx.clear();
-                sys.eachEdge(function (edge, p1, p2) {
-                    gfx.line(p1, p2, {stroke: "#b2b19d", width: 2});
-                });
-                sys.eachNode(function (node, pt) {
-                    var name;
-                    if (node.data.label == 'osoba') {
-                        name = node.data.data.imiona + ' ' + node.data.data.nazwisko;
-                    } else if (node.data.label == 'podmiot') {
-                        name = node.data.data.nazwa;
-                    }
-                    var w = Math.max(20, gfx.textWidth(name) * .7);
-                    if (node.data.shape == 'dot') {
-                        gfx.oval(pt.x - w / 2, pt.y - w / 2, w, w, {fill: node.data.color, radius: node.data.radius});
-                        gfx.text(name, pt.x, pt.y + 7, {color: "white", align: "center", font: "Arial", size: 8});
-                        //gfx.text(name, pt.x, pt.y + 7, {color: "white", align: "center", font: "Arial", size: 10});
-                    } else {
-                        gfx.rect(pt.x - w / 2, pt.y - 8, w, 20, 4, {fill: node.data.color, radius: node.data.radius});
-                        gfx.text(name, pt.x, pt.y + 9, {color: "white", align: "center", font: "Arial", size: 8});
-                        //gfx.text(name, pt.x, pt.y + 9, {color: "white", align: "center", font: "Arial", size: 10});
-                    }
-                });
-            },
-            switchMode: function (e) {
-                if (e.mode == 'hidden') {
-                    dom.stop(true).fadeTo(e.dt, 0, function () {
-                        if (sys) sys.stop();
-                        $(this).hide();
-                    })
-                } else if (e.mode == 'visible') {
-                    dom.stop(true).css('opacity', 0).show().fadeTo(e.dt, 1, function () {
-                        that.resize()
-                    });
-                    if (sys) sys.start()
-                }
-            },
-            switchSection: function (newSection) {
-                var parent = $.map(sys.getEdgesTo(newSection), function (edge) {
-                    return edge.target;
-                });
-                var children = $.map(sys.getEdgesFrom(newSection), function (edge) {
-                    return edge.target;
-                });
-                console.log(parent, children);
-            },
-            _initMouseHandling: function () {
-                selected = null;
-                nearest = null;
-                var dragged = null;
-
-                var handler = {
-                    moved: function (e) {
-                        var pos = $(canvas).offset();
-                        _mouseP = arbor.Point(e.pageX - pos.left, e.pageY - pos.top);
-                        nearest = sys.nearest(_mouseP);
-                        if (!nearest.node) return false;
-
-                        sys.tweenNode(nearest, 3, {radius: 1});
-
-                        return false
-                    },
-                    clicked: function (e) {
-                        var pos = $(canvas).offset();
-                        _mouseP = arbor.Point(e.pageX - pos.left, e.pageY - pos.top);
-                        nearest = dragged = sys.nearest(_mouseP);
-
-                        sys.tweenNode(nearest, 3, {radius: 1});
-
-                        if (dragged && dragged.node !== null) dragged.node.fixed = true;
-
-                        $(canvas).unbind('mousemove', handler.moved);
-                        $(canvas).bind('mousemove', handler.dragged);
-                        $(window).bind('mouseup', handler.dropped);
-
-                    },
-                    dragged: function (e) {
-                        var pos = $(canvas).offset();
-                        var s = arbor.Point(e.pageX - pos.left, e.pageY - pos.top);
-
-                        if (!nearest) return;
-                        if (dragged !== null && dragged.node !== null) {
-                            dragged.node.p = sys.fromScreen(s)
-                        }
-                    },
-
-                    dropped: function () {
-                        if (dragged === null || dragged.node === undefined) return;
-                        if (dragged.node !== null) dragged.node.fixed = false;
-                        dragged.node.tempMass = 50;
-                        dragged = null;
-                        $(canvas).unbind('mousemove', handler.dragged);
-                        $(window).unbind('mouseup', handler.dropped);
-                        $(canvas).bind('mousemove', handler.moved);
-                        _mouseP = null;
-                    }
-                };
-
-                $(canvas).mousedown(handler.clicked);
-                $(canvas).mousemove(handler.moved);
-
+            size: {
+                'links': 150,
+                'nodes': 50
             }
         };
+        d3Data.force = d3.layout.force().linkDistance(d3Data.size.links).linkStrength(2).size([d3Data.width, d3Data.height]);
+        d3Data.svg = d3.select("#connectionGraph").append("svg").attr("width", d3Data.width).attr("height", d3Data.height);
 
-        return that;
-    };
-})(jQuery);
+        d3.json(connectionGraph.data('id') + "/graph.json", function (error, results) {
+            var graph = results._layers.graph;
+
+            var nodes = graph.nodes.slice(),
+                links = [];
+
+            graph.relationships.forEach(function (link) {
+                var s = jQuery.grep(nodes, function (e) {
+                        return e.id == link.start;
+                    })[0],
+                    t = jQuery.grep(nodes, function (e) {
+                        return e.id == link.end;
+                    })[0];
+                links.push({source: s, target: t});
+            });
+
+            d3Data.force
+                .nodes(nodes)
+                .links(links)
+                .start();
+
+            var link = d3Data.svg.selectAll(".link")
+                .data(links)
+                .enter().append("line")
+                .attr("class", "link")
+                .style({"stroke-width": 1, 'stroke': d3Data.color.links});
+
+            var node = d3Data.svg.selectAll(".node")
+                    .data(nodes)
+                    .enter().append("g")
+                    .attr("class", "node")
+                    .attr("transform", function (d) {
+                        return "translate(" + d.x + "," + d.y + ")";
+                    })
+            //.call(d3Data.force.drag)
+                ;
+
+            node.append("circle")
+                .attr("r", d3Data.size.nodes)
+                .style("fill", function (d) {
+                    return d3Data.color[d.label]
+                });
+
+            node.append("text")
+                .attr("dy", ".3em")
+                .attr('class', 'nodeText')
+                .style("text-anchor", "middle")
+                .text(function (d) {
+                    var name = '';
+                    if (d.label == 'podmiot')
+                        name = d.data.nazwa;
+                    else if (d.label == 'osoba')
+                        name = d.data.imiona + ' ' + d.data.nazwisko;
+                    return name.substring(0, d3Data.size.nodes / 3);
+                })
+                .style("fill-opacity", 1);
+
+            d3Data.force.on("tick", function () {
+                link.attr("x1", function (d) {
+                    return d.source.x;
+                })
+                    .attr("y1", function (d) {
+                        return d.source.y;
+                    })
+                    .attr("x2", function (d) {
+                        return d.target.x;
+                    })
+                    .attr("y2", function (d) {
+                        return d.target.y;
+                    });
+
+                node.attr("cx", function (d) {
+                    return d.x;
+                })
+                    .attr("cy", function (d) {
+                        return d.y;
+                    });
+            });
+        });
+    }
+});
