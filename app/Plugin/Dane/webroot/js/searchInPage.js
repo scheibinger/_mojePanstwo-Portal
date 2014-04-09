9a606a3ad3125041194ea00bdc2810420553491b
Cain-Ish <mariusz.machuta@gmail.com>
2014-04-09 13:40:43 +0200

    search in page

---
+++ app/Plugin/Dane/webroot/js/searchInPage.js
@@ -0,0 +1,88 @@
+(function ($) {
+    var searchInPage = $('#searchInPage'),
+        searchInPageNav = searchInPage.find('.searchInPageContent .nav');
+
+    function searchAndHighlight(button, searchTerm) {
+        if (searchTerm) {
+            //var wholeWordOnly = new RegExp("\\g"+searchTerm+"\\g","ig"); //matches whole word only
+            //var anyCharacter = new RegExp("\\g["+searchTerm+"]\\g","ig"); //matches any word with any of search chars characters
+            var selector = ".objectsPageWindow" || "body",                             //use body as selector if none provided
+                searchTermRegEx = new RegExp("(" + searchTerm + ")", "gi"),
+                match = 0,
+                helper = {};
+
+            helper.doHighlight = function (node, searchTerm) {
+                if (node.nodeType === 3) {
+                    if (node.nodeValue.match(searchTermRegEx)) {
+                        match++;
+                        var tempNode = document.createElement('span');
+                        tempNode.innerHTML = node.nodeValue.replace(searchTermRegEx, '<span class="markedSearchWord">$1</span>');
+                        node.parentNode.insertBefore(tempNode, node);
+                        node.parentNode.removeChild(node);
+                    }
+                }
+                else if (node.nodeType === 1 && node.childNodes && !/(style|script)/i.test(node.tagName)) {
+                    $.each(node.childNodes, function (i, v) {
+                        helper.doHighlight(node.childNodes[i], searchTerm);
+                    });
+                }
+            };
+
+            $.each($(selector).children(), function (index, val) {
+                helper.doHighlight(this, searchTerm);
+            });
+
+            button.data('searchMatch', match);
+        }
+        return false;
+    }
+
+    searchInPage.find('a.slider').click(function () {
+        var button = $(this);
+        if (button.hasClass('closed')) {
+            button.removeClass('closed');
+            searchInPage.animate({
+                left: searchInPage.data('left')
+            })
+        } else {
+            button.addClass('closed');
+            searchInPage.data('left', searchInPage.css('left'));
+            searchInPage.animate({
+                left: -(searchInPage.width() - button.width())
+            })
+        }
+    });
+    searchInPageNav.find(' > li > a').click(function (e) {
+        var that = $(this),
+            markedSearchWord = "markedSearchWord";
+
+        e.preventDefault();
+
+        if (!that.parent().hasClass('active')) {
+            if (searchInPageNav.find('li.active').length > 0) {
+                searchInPageNav.find('li.active').removeClass('active');
+
+                $('.' + markedSearchWord).removeClass(markedSearchWord);     //Remove old search highlights
+            }
+
+            /*ADD ACTIVE MARKER AT CURRENT ONE*/
+            that.parent().addClass('active');
+            that.data('searchNodesPos', -1);
+
+            searchAndHighlight(that, $.trim(that.text()));
+        }
+
+        var newPos = Number(that.data('searchNodesPos')) + 1;
+
+        if (newPos >= Number(that.data('searchMatch')))
+            newPos = 0;
+
+        $('html, body').animate({
+            scrollTop: $($('.' + markedSearchWord)[newPos]).offset().top - 80
+        }, 2000);
+
+        that.data('searchNodesPos', newPos);
+
+    });
+}(jQuery));
+
