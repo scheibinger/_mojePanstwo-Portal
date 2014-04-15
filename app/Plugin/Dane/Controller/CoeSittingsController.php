1b1a468b5e433ac95130bc9824cd4ce069bba246
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-04-15 08:53:35 +0200

    Care-o-meter button in CoeSittingsController

--- app/Plugin/Dane/Controller/CoeSittingsController.php
+++ app/Plugin/Dane/Controller/CoeSittingsController.php
@@ -5,7 +5,11 @@ App::uses('DataobjectsController', 'Dane.Controller');
 class CoeSittingsController extends DataobjectsController
 {
     public $menu = array();
-
+	
+	public $objectOptions = array(
+        'buttons' => array('shoutIt', 'careIt'),
+    );
+	
     public function view()
     {
         parent::view();
