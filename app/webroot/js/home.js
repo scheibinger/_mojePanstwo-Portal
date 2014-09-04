247907ff98b1d60396cc6c730d6d0010ba0d9a58
CainIsh <mariusz.machuta@gmail.com>
2014-09-04 11:46:54 +0200

    searchHome

--- app/webroot/js/home.js
+++ app/webroot/js/home.js
@@ -2,7 +2,7 @@ jQuery(function () {
     jQuery('#home').find('.apps .appFolder').click(function (event) {
         event.preventDefault();
         _mojePanstwoCockpitSlider.showDialogBox(event);
-    })
+    });
 
     var globalSearch;
 
@@ -11,58 +11,61 @@ jQuery(function () {
             globalSearchBtn = globalSearch.find('.input-group-btn .btn'),
             globalSearchCache = {};
 
-        console.log(globalSearchBtn);
-
         globalSearchInput.autocomplete({
             minLength: 2,
-            delay: 150,
+            delay: 100,
             source: function (request, response) {
                 var term = request.term;
                 if (term in globalSearchCache) {
                     response(globalSearchCache[ term ]);
-                    return;
-                }
-                globalSearchBtn.addClass('loading');
-                $.getJSON("/dane/suggest.json?q=" + request.term, function (data, status, xhr) {
-                    globalSearchBtn.removeClass('loading');
-                    var results = $.map(data.hits, function (item) {
-                        
-                        var label = '<span class="col-md-2">' + item.dataset + '</span><span class="col-md-10">' + item.title + '</span>';
-                        
-                        return {
-                            label: label,
-                            title: item.title,
-                            value: item.id,
-                            dataset: item.dataset
-                        };
+                } else {
+                    globalSearchBtn.addClass('loading');
+                    $.getJSON("/dane/suggest.json?q=" + request.term, function (data, status, xhr) {
+                        globalSearchBtn.removeClass('loading');
+                        var results = $.map(data.hits, function (item) {
+
+                            return {
+                                title: item.title,
+                                value: item.id,
+                                dataset: item.dataset
+                            };
+                        });
+                        if (results.length == 0)
+                            results = [
+                                {label: _mPHeart.translation.LC_SEARCH_BRAK_WYNIKOW, value: null}
+                            ];
+                        globalSearchCache[ term ] = results;
+                        response(results);
                     });
-                    if (results.length == 0)
-                        results = [
-                            {label: _mPHeart.translation.LC_SEARCH_BRAK_WYNIKOW, value: null}
-                        ];
-                    globalSearchCache[ term ] = results;
-                    response(results);
-                });
+                }
             },
             focus: function (event, ui) {
                 if (ui.item.value !== null)
                     globalSearchInput.val(ui.item.title);
                 return false;
             },
+            open: function (event, ui) {
+                var uiAutocomplete = $('.ui-autocomplete');
+                uiAutocomplete.css({'top': parseInt(uiAutocomplete.css('top')) - 6 + 'px', 'width': globalSearchInput.outerWidth()});
+            },
             select: function (event, ui) {
                 if (ui.item.value !== null) {
-                    globalSearchInput.val(ui.item.label);
+                    globalSearchInput.val(ui.item.title);
                 }
                 return false;
             }
-        }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-	      return $( "<li class=\"row\">" )
-	        .append( "<a href=\"/dane/" + item.dataset + "/" + item.value + "\">" + item.label + "</a>" )
-	        .appendTo( ul );
-	    };
-	    
-	    // ui-menu-item
-	    // presentation
-	    
+        }).data("ui-autocomplete")._renderItem = function (ul, item) {
+            return $('<li></li>').addClass("row")
+                .append(
+                    $('<a></a>').attr({'href': '/dane/' + item.dataset + '/' + item.value, 'target': '_blank'})
+                        .append(
+                            $('<span></span>').addClass('col-md-2').text(item.dataset)
+                        )
+                        .append(
+                            $('<span></span>').addClass('col-md-10').text(item.title)
+                        )
+                )
+                .appendTo(ul);
+        };
     }
 });
