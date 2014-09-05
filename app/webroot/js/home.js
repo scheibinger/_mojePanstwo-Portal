4aa8dc6ed43caa66f8fe85eb23a25311bed2ae50
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-09-05 10:50:50 +0200

    elasticSearch implementation

--- app/webroot/js/home.js
+++ app/webroot/js/home.js
@@ -13,7 +13,7 @@ jQuery(function () {
 
         globalSearchInput.autocomplete({
             minLength: 2,
-            delay: 100,
+            delay: 200,
             source: function (request, response) {
                 var term = globalSearchWord = request.term;
                 if (term in globalSearchCache) {
@@ -21,6 +21,7 @@ jQuery(function () {
                 } else {
                     globalSearchBtn.addClass('loading');
                     $.getJSON("/dane/suggest.json?q=" + request.term, function (data, status, xhr) {
+                        
                         var results = $.map(data.hits, function (item) {
                             var shortTitleLimit = 200,
                                 shortTitle = '';
@@ -35,32 +36,48 @@ jQuery(function () {
                                 title: item.title,
                                 shortTitle: shortTitle,
                                 value: item.id,
-                                dataset: item.dataset
+                                dataset: item.dataset,
+                                label: item.label
                             };
                         });
-                        if (results.length == 0)
-                            results = [
-                                {
-                                    title: _mPHeart.translation.LC_SEARCH_BRAK_WYNIKOW,
-                                    shortTitle: _mPHeart.translation.LC_SEARCH_BRAK_WYNIKOW,
-                                    value: null,
-                                    dataset: null
-                                }
-                            ];
+                        
                         globalSearchCache[ term ] = results;
-                        response(results);
+                        
+                        if (results.length == 0) {
+                            
+                            console.log('close');
+                            $('.ui-autocomplete').hide();
+                            globalSearchInput.removeClass('open');
+                            globalSearchBtn.removeClass('loading');
+                                                        
+                            
+                        } else {
+                        
+	                        response(results);
+                        
+                        }
                     });
                 }
             },
+            /*
             focus: function (event, ui) {
                 if (ui.item.value !== null)
                     globalSearchInput.val(ui.item.title);
                 return false;
             },
+            */
             open: function (event, ui) {
+                        	
                 var uiAutocomplete = $('.ui-autocomplete');
-                uiAutocomplete.css({'top': parseInt(uiAutocomplete.css('top')) - 6 + 'px', 'width': globalSearchInput.outerWidth()});
+                uiAutocomplete.css({'top': parseInt(uiAutocomplete.css('top'))-1 + 'px', 'width': globalSearchInput.outerWidth()-1});
+                globalSearchInput.addClass('open');
                 globalSearchBtn.removeClass('loading');
+                
+            },
+            close: function() {
+	          
+	          globalSearchInput.removeClass('open');
+	            
             },
             select: function (event, ui) {
                 if (ui.item.value !== null) {
@@ -73,10 +90,10 @@ jQuery(function () {
                 .append(
                     $('<a></a>').attr({'href': '/dane/' + item.dataset + '/' + item.value, 'target': '_blank'})
                         .append(
-                            $('<span></span>').addClass('col-md-2').text(item.dataset)
+                            $('<p></p>').addClass('col-md-2').addClass('_label').html('<span class="label label-default label-sm">' + item.label + '</span>')
                         )
                         .append(
-                            $('<span></span>').addClass('col-md-10').text(item.shortTitle)
+                            $('<p></p>').addClass('col-md-10').addClass('_title').text(item.shortTitle)
                         )
                 )
                 .appendTo(ul);
