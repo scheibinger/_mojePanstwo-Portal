98f43e6ed6e69a3367965bc41b36cf31dee9ffd6
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-09-12 18:50:21 +0200

    #283

--- app/Plugin/Dane/Controller/PowiatyController.php
+++ app/Plugin/Dane/Controller/PowiatyController.php
@@ -4,14 +4,51 @@ App::uses('DataobjectsController', 'Dane.Controller');
 
 class PowiatyController extends DataobjectsController
 {
+    
     public function view()
     {
-        parent::_prepareView();
-
-        $this->innerSearch('gminy', array(
-            'powiat_id' => $this->object->object_id,
-        ), array(
-            'searchTitle' => sprintf(__d('dane', 'LC_DANE_GMINY_W_POWIECIE'), $this->object->getData('nazwa')),
+    	
+    	parent::_prepareView();
+    	
+    	if( ($this->object->getData('typ_id')=='2') || ($this->object->getData('typ_id')=='3') ) {
+	    	if( $gmina_id = $this->object->loadLayer('gmina') ) {
+		    	
+		    	$this->redirect('/dane/gminy/' . $gmina_id);
+		    	
+	    	}
+    	}
+    	
+        $this->dataobjectsBrowserView(array(
+            'source' => 'powiaty.gminy:' . $this->object->getId(),
+            'dataset' => 'gminy',
+            'excludeFilters' => array(
+                'wojewodztwo_id', 'powiat_id'
+            ),
         ));
+        
+        $this->set('title_for_layout', 'Gminy w powiecie ' . ' ' . $this->object->getData('nazwa'));
+       
     }
+    
+    public function beforeRender()
+    {
+
+        // PREPARE MENU		
+        $href_base = '/dane/powiaty/' . $this->request->params['id'];
+
+        $menu = array(
+            'items' => array(
+                array(
+                    'id' => '',
+                    'href' => $href_base,
+                    'label' => 'Gminy',
+                ),
+            )
+        );      
+
+        $menu['selected'] = ($this->request->params['action'] == 'view') ? '' : $this->request->params['action'];
+        $this->set('_menu', $menu);
+
+    }
+    
 } 
